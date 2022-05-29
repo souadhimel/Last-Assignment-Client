68296a5f8daaffbf77f272258d645275274f7e34
souadhimel <souad.anam@gmail.com>
2022-05-29 20:12:22 +0600

    dashboard

--- src/components/Pages/Purchase/Purchase.js
+++ src/components/Pages/Purchase/Purchase.js
@@ -42,7 +42,7 @@ const { register, handleSubmit, reset } = useForm();
           timer: 1500
         })
         reset()
-        navigate('/home')
+        navigate('/dashboard')
       });
   console.log(data);
   
